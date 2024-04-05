eb8d2639a3e11acb3ee953d99eabd6c888703289
amitopfo <ngyuheng555@gmail.com>
2024-04-05 18:43:37 +0800

    a

--- scripts/2createFlightHour.sql
+++ scripts/2createFlightHour.sql
@@ -1,8 +1,8 @@
 CREATE TABLE FLIGHT_HOUR (
     pilot_id NUMBER NOT NULL,
-    aircraft_model_id NUMBER NOT NULL,
+    aircraft_model_id VARCHAR(4) NOT NULL,
     flight_hour NUMBER,
     PRIMARY KEY (pilot_id,aircraft_model_id),
     FOREIGN KEY (pilot_id) REFERENCES PILOT(pilot_id),
     FOREIGN KEY (aircraft_model_id) REFERENCES AIRCRAFT_MODEL(aircraft_model_id)
-)
+);
