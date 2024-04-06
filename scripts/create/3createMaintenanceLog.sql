6922447176a8f515829e34023045b326e811ea0f
amitopfo <ngyuheng555@gmail.com>
2024-04-06 22:08:23 +0800

    CREATE MAINTENANCE LOG

---
+++ scripts/create/3createMaintenanceLog.sql
@@ -0,0 +1,10 @@
+CREATE TABLE MAINTENANCE_LOG(
+    maintenance_log_id NUMBER NOT NULL,
+    aircraft_id VARCHAR(10) NOT NULL,
+    maintenance_type_id NUMBER NOT NULL,
+    maintenance_date_time DATE NOT NULL,
+    maintenance_description VARCHAR(200),
+    PRIMARY KEY (maintenance_log_id),
+    FOREIGN KEY (maintenance_type_id) REFERENCES MAINTENANCE_TYPE(maintenance_type_id),
+    FOREIGN KEY (aircraft_id) REFERENCES AIRCRAFT(aircraft_id)
+)
