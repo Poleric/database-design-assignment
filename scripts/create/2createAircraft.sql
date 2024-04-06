19dd5efb46e94cfa8ba6df1b64563b288d3b76ce
amitopfo <ngyuheng555@gmail.com>
2024-04-06 21:45:22 +0800

    create aircraft

--- scripts/create/2createAircraft.sql
+++ scripts/create/2createAircraft.sql
@@ -1,6 +1,6 @@
 CREATE TABLE AIRCRAFT (
-    aircraft_id NUMBER NOT NULL,
-    aircraft_model_id NUMBER NOT NULL,
+    aircraft_id VARCHAR(10) NOT NULL,
+    aircraft_model_id VARCHAR(4) NOT NULL,
     capacity NUMBER(10,2) NOT NULL,
     purchase_date DATE NOT NULL,
     PRIMARY KEY (aircraft_id),
