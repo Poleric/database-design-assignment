0355aa350c851aafeddf1208021562954323bbab
amitopfo <ngyuheng555@gmail.com>
2024-04-06 21:45:58 +0800

    create membership

---
+++ scripts/1createMembership.sql
@@ -0,0 +1,10 @@
+CREATE TABLE MEMBERSHIP (
+    membership_id       NUMBER,
+    account_id          NUMBER  NOT NULL,
+    membership_type_id  NUMBER  NOT NULL,
+    accumulated_points  NUMBER  DEFAULT 0,
+    PRIMARY KEY (membership_id),
+    FOREIGN KEY (account_id)         REFERENCES ACCOUNT(account_id),
+    FOREIGN KEY (membership_type_id) REFERENCES MEMBERSHIP_TYPE(membership_type_id)
+);
+
