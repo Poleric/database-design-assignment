4df2d9a9689de400688e82509df29bffa501deef
amitopfo <ngyuheng555@gmail.com>
2024-04-09 12:19:27 +0800

    flight sequence

--- scripts/create/4createFlightSequence.sql
+++ scripts/create/4createFlightSequence.sql
@@ -1,5 +1,5 @@
 CREATE TABLE FLIGHT_SEQUENCE(
-    flight_ticket_id VARCHAR(10) NOT NULL,
+    flight_ticket_id NUMBER NOT NULL,
     flight_id VARCHAR(10) NOT NULL,
     SEAT_ID NUMBER NOT NULL,
     flight_sequence NUMBER NOT NULL,
