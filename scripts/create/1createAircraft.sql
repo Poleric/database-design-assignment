5b4be7e08e8e03ebafdbb91ee25a158eda180c80
Poleric <nixonleongkb@gmail.com>
2024-04-10 19:43:55 +0800

    update aircraft

--- scripts/create/1createAircraft.sql
+++ scripts/create/1createAircraft.sql
@@ -1,8 +1,8 @@
 CREATE TABLE AIRCRAFT (
-    aircraft_id VARCHAR(10) NOT NULL,
-    aircraft_model_id VARCHAR(4) NOT NULL,
-    capacity NUMBER(10,2) NOT NULL,
-    purchase_date DATE NOT NULL,
+    aircraft_id        VARCHAR(10)  NOT NULL,
+    aircraft_model_id  VARCHAR(4)   NOT NULL,
+    capacity           NUMBER(3)    NOT NULL,
+    purchase_date      DATE         NOT NULL,
     PRIMARY KEY (aircraft_id),
     FOREIGN KEY (aircraft_model_id) REFERENCES AIRCRAFT_MODEL(aircraft_model_id)
 );
