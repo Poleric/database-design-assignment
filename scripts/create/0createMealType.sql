2e79038a6ccc0b2544a52747a74137ad975628ad
Poleric <nixonleongkb@gmail.com>
2024-04-10 15:02:37 +0800

    update meal type

--- scripts/create/0createMealType.sql
+++ scripts/create/0createMealType.sql
@@ -1,6 +1,6 @@
 CREATE TABLE MEAL_TYPE(
-    meal_type_id VARCHAR(10),
-    meal_type_name VARCHAR(20),
-    meal_type_desciption VARCHAR(200),
+    meal_type_id           VARCHAR(4)    NOT NULL,
+    meal_type_name         VARCHAR(20)   NOT NULL,
+    meal_type_description  VARCHAR(100),
     PRIMARY KEY (meal_type_id)
-)
+);
