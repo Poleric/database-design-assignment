43b9253b14bae4459576bd10b99b2d7a9d69993e
Poleric <nixonleongkb@gmail.com>
2024-04-11 00:15:22 +0800

    create route

--- scripts/create/2createRoute.sql
+++ scripts/create/2createRoute.sql
@@ -1,3 +1,6 @@
 CREATE TABLE ROUTE (
-    route_id 
-)
+    route_id           VARCHAR(7) PRIMARY KEY,
+    from_airport_id    VARCHAR(3) NOT NULL REFERENCES AIRPORT(airport_id),
+    to_airport_id      VARCHAR(3) NOT NULL REFERENCES AIRPORT(airport_id),
+    route_description  VARCHAR(100)
+);
