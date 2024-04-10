ffce76e09e568cdbdd3bfc8abd42bf00353b2117
Poleric <nixonleongkb@gmail.com>
2024-04-10 14:57:04 +0800

    make check inline

--- scripts/create/0createAccount.sql
+++ scripts/create/0createAccount.sql
@@ -5,8 +5,7 @@ CREATE TABLE ACCOUNT (
     joined_date      TIMESTAMP     NOT NULL,
     email_address    VARCHAR(254)  NOT NULL,
     phone_number     VARCHAR(20),
-    gender           CHAR(1),
+    gender           CHAR(1)       CHECK (gender = 'M' OR gender = 'F' OR gender IS NULL),
     PRIMARY KEY (account_id),
-    CONSTRAINT check_email CHECK (REGEXP_LIKE(email_address,'^(?:[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$')),  -- http://emailregex.com/
-    CONSTRAINT check_gender CHECK (gender = 'M' OR gender = 'F')
+    CONSTRAINT check_email CHECK (REGEXP_LIKE(email_address,'^(?:[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$'))  -- http://emailregex.com/
 );
--- scripts/create/0createEmployee.sql
+++ scripts/create/0createEmployee.sql
@@ -1,10 +1,8 @@
 CREATE TABLE EMPLOYEE (
     employee_id    NUMBER        NOT NULL,
     employee_name  VARCHAR(255)  NOT NULL,
-    gender         CHAR(1)       NOT NULL,
+    gender         CHAR(1)       CHECK (gender = 'M' OR gender = 'F' OR gender IS NULL),
     hired_date     DATE          NOT NULL,
-    salary         NUMBER(7,2)   NOT NULL,
-    PRIMARY KEY (employee_id),
-    CONSTRAINT check_gender CHECK (gender = 'M' OR gender = 'F'),
-    CONSTRAINT check_salary_not_neg CHECK (salary > 0)
+    salary         NUMBER(7,2)   NOT NULL CHECK (salary > 0),
+    PRIMARY KEY (employee_id)
 );
