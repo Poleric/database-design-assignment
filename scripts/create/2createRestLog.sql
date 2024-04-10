1e541cd2b84b33d44e24c9fc3f6946753a3f5622
Poleric <nixonleongkb@gmail.com>
2024-04-10 22:33:19 +0800

    update rest log

--- scripts/create/2createRestLog.sql
+++ scripts/create/2createRestLog.sql
@@ -1,8 +1,8 @@
 CREATE TABLE REST_LOG (
-    rest_id NUMBER NOT NULL,
-    pilot_id NUMBER NOT NULL,
-    start_date_time DATE NOT NULL,
-    end_date_time DATE NOT NULL,
+    rest_id         NUMBER NOT NULL,
+    pilot_id        NUMBER NOT NULL,
+    start_datetime  TIMESTAMP NOT NULL,
+    end_datetime    TIMESTAMP NOT NULL,
     PRIMARY KEY (rest_id),
     FOREIGN KEY (pilot_id) REFERENCES PILOT (pilot_id)
-)
+);
