12a5dbeecb7cc495fb146f6e2b60ef716e56c4e8
Poleric <nixonleongkb@gmail.com>
2024-04-11 00:38:59 +0800

    create offer claims

--- scripts/create/3createOfferClaims.sql
+++ scripts/create/3createOfferClaims.sql
@@ -1,8 +1,6 @@
 CREATE TABLE OFFER_CLAIMS(
-    payment_id VARCHAR(10) NOT NULL,
-    offer_id VARCHAR(10) NOT NULL,
-    date_claimed DATE,
-    PRIMARY KEY(ticket_payment_id,offer_id),
-    FOREIGN KEY(ticket_payment_id) REFERENCES TICKET_PAYMENT(payment_id),
-    FOREIGN KEY(offer_id) REFERENCES OFFER(offer_id)
-)
+    payment_id        VARCHAR(10) REFERENCES PAYMENT(payment_id),
+    offer_id          VARCHAR(10) REFERENCES OFFER(offer_id),
+    datetime_claimed  TIMESTAMP NOT NULL,
+    PRIMARY KEY (payment_id, offer_id)
+);
