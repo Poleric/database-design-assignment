d4b72249ed1ed05b93da80466b151a3d01eef3c1
Poleric <nixonleongkb@gmail.com>
2024-04-11 00:34:50 +0800

    change offer id to varchar

--- scripts/create/1createOffer.sql
+++ scripts/create/1createOffer.sql
@@ -1,5 +1,5 @@
 CREATE TABLE OFFER(
-    offer_id       NUMBER        PRIMARY KEY,
+    offer_id       VARCHAR(10)   PRIMARY KEY,
     account_id     NUMBER        NOT NULL REFERENCES ACCOUNT(account_id),
     offer_details  VARCHAR(100),
     discount_rate  NUMBER(3)     NOT NULL CHECK (discount_rate BETWEEN 0 AND 100),
