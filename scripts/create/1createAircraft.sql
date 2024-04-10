36eda726889a4c7a6db88f5011f7313d6b4a4269
Poleric <nixonleongkb@gmail.com>
2024-04-10 19:56:38 +0800

    add unique

--- scripts/create/1createAircraft.sql
+++ scripts/create/1createAircraft.sql
@@ -1,6 +1,6 @@
 CREATE TABLE AIRCRAFT (
     aircraft_id        VARCHAR(10)  NOT NULL,
-    aircraft_model_id  VARCHAR(4)   NOT NULL,
+    aircraft_model_id  VARCHAR(4)   NOT NULL UNIQUE,
     capacity           NUMBER(3)    NOT NULL,
     purchase_date      DATE         NOT NULL,
     PRIMARY KEY (aircraft_id),
