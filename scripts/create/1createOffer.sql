3978420cf78dcfe375e7736933097e902b274904
Poleric <nixonleongkb@gmail.com>
2024-04-10 22:07:58 +0800

    update offer

--- scripts/create/1createOffer.sql
+++ scripts/create/1createOffer.sql
@@ -1,10 +1,9 @@
 CREATE TABLE OFFER(
-    offer_id NUMBER NOT NULL,
-    account_id NUMBER NOT NULL,
-    offer_details VARCHAR(100),
-    conditions VARCHAR(50),
-    discount NUMBER(5,2),
-    expiry_date DATE,
+    offer_id       NUMBER        NOT NULL,
+    account_id     NUMBER        NOT NULL,
+    offer_details  VARCHAR(100),
+    discount_rate  NUMBER(3)     NOT NULL CHECK (discount_rate BETWEEN 0 AND 100),
+    expiry_date    DATE          NOT NULL,
     PRIMARY KEY(offer_id),
     FOREIGN KEY(account_id) REFERENCES ACCOUNT(account_id)
-)
+);
