74edd468032cd695ed9378c74cb393134261ee88
Poleric <nixonleongkb@gmail.com>
2024-04-10 17:17:10 +0800

    remove meal type

--- scripts/create/0createMealType.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE MEAL_TYPE(
-    meal_type_id           VARCHAR(4)    NOT NULL,
-    meal_type_name         VARCHAR(20)   NOT NULL,
-    meal_type_description  VARCHAR(100),
-    PRIMARY KEY (meal_type_id)
-);
