b6a11caeb658a06810e521b1651d8d886f8791ec
Poleric <nixonleongkb@gmail.com>
2024-04-10 21:52:32 +0800

    update pilot

--- scripts/create/1createPilot.sql
+++ scripts/create/1createPilot.sql
@@ -1,6 +1,6 @@
 CREATE TABLE PILOT (
-    pilot_id NUMBER NOT NULL,
-    employee_id NUMBER NOT NULL,
+    pilot_id     NUMBER NOT NULL,
+    employee_id  NUMBER NOT NULL,
     PRIMARY KEY (pilot_id),
     FOREIGN KEY (employee_id) REFERENCES EMPLOYEE(employee_id)
-)
+);
