a46ce4f3e8e1185d6334a2fbc78144076bf62133
Poleric <nixonleongkb@gmail.com>
2024-04-11 00:31:02 +0800

    create flight ticket

--- scripts/create/3createFlightTicket.sql
+++ scripts/create/3createFlightTicket.sql
@@ -1,9 +1,9 @@
 CREATE TABLE FLIGHT_TICKET(
-    flight_ticket_id NUMBER NOT NULL,
-    account_id NUMBER NOT NULL,
-    date_placed DATE NOT NULL,
-    boarding_group VARCHAR(20),
-    ticket_price NUMBER(7,2),
-    PRIMARY KEY (flight_ticket_id),
-    FOREIGN KEY (account_id) REFERENCES ACCOUNT(account_id)
+    flight_ticket_id    VARCHAR(15)  PRIMARY KEY,
+    account_id          NUMBER       NOT NULL  REFERENCES ACCOUNT(account_id),
+    payment_id          VARCHAR(10)  NOT NULL  REFERENCES PAYMENT(payment_id),
+    datetime_placed     TIMESTAMP    NOT NULL,
+    boarding_group      NUMBER       NOT NULL  CHECK (boarding_group > 0),
+    ticket_price        NUMBER(7,2)  NOT NULL,
+    special_service_id  VARCHAR(4)             REFERENCES SPECIAL_SERVICE(special_service_id)
 );
