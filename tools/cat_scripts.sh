7652c3361f54315847bc62f31ffc412d20daaf6f
Poleric <nixonleongkb@gmail.com>
2024-04-10 22:16:43 +0800

    update cat script

--- tools/cat_scripts.sh
+++ tools/cat_scripts.sh
@@ -5,6 +5,9 @@ cd "$(dirname "$0")" || return
 
 cd ../scripts/ || return
 
+# remove old
+rm create_all.sql insert_all.sql run.sql
+
 awk '{print $0}' ./create/*.sql > create_all.sql
 awk '{print $0}' ./insert/*.sql > insert_all.sql
 
