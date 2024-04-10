a53c698f0f7e03333db54c675df6c39c40b02d9b
Poleric <nixonleongkb@gmail.com>
2024-04-10 22:31:10 +0800

    update payment

--- scripts/create/2createPayment.sql
+++ scripts/create/2createPayment.sql
@@ -1,11 +1,8 @@
-CREATE TABLE TICKET_PAYMENT(
-    payment_id VARCHAR(10) NOT NULL,
-    transaction_id NUMBER NOT NULL,
-    account_id NUMBER NOT NULL,
-    payment_amount NUMBER(7,2),
-    reference_ VARCHAR(50),
-    remark_ VARCHAR(100),
+CREATE TABLE PAYMENT(
+    payment_id      VARCHAR(10) NOT NULL,
+    transaction_id  VARCHAR(10) NOT NULL,
+    account_id      NUMBER      NOT NULL,
     PRIMARY KEY(payment_id),
     FOREIGN KEY(transaction_id) REFERENCES TRANSACTION(transaction_id),
-    FOREIGN KEY(account_id) REFERENCES ACCOUNT(account_id)
-)
+    FOREIGN KEY(account_id)     REFERENCES ACCOUNT(account_id)
+);
