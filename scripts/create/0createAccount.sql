33d2fce2322795299ec64848ac3d8bb3b14a209c
Poleric <nixonleongkb@gmail.com>
2024-04-10 14:11:11 +0800

    update account

--- scripts/create/0createAccount.sql
+++ scripts/create/0createAccount.sql
@@ -1,10 +1,11 @@
 CREATE TABLE ACCOUNT (
     account_id       NUMBER,
     username         VARCHAR(255)  NOT NULL,
-    hashed_password  VARCHAR(64)   NOT NULL,
-    joined_date      DATE          NOT NULL,
-    email_address    VARCHAR(255)  NOT NULL,
+    hashed_password  VARCHAR(60)   NOT NULL,
+    joined_date      TIMESTAMP     NOT NULL,
+    email_address    VARCHAR(254)  NOT NULL,
     phone_number     VARCHAR(20),
+    gender           CHAR(1),
     PRIMARY KEY (account_id),
     CONSTRAINT check_email CHECK (REGEXP_LIKE(email_address,'^(?:[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$'))  -- http://emailregex.com/
 );
