e99188e202b5789499cb78fa43418b0bb8330b03
Poleric <nixonleongkb@gmail.com>
2024-04-10 14:32:27 +0800

    add special service

---
+++ scripts/create/0createSpecialService.sql
@@ -0,0 +1,6 @@
+CREATE TABLE SPECIAL_SERVICE (
+    special_service_id           VARCHAR(4)    NOT NULL,
+    special_service_name         VARCHAR(20)   NOT NULL,
+    special_service_description  VARCHAR(100),
+    PRIMARY KEY (special_service_id)
+);
