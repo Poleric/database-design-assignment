fe9205c6a93b989d4054781639f1d9dad4ed8462
Poleric <nixonleongkb@gmail.com>
2024-04-10 22:03:37 +0800

    change foreign type id to varchar(4)

--- scripts/create/1createMembership.sql
+++ scripts/create/1createMembership.sql
@@ -1,8 +1,8 @@
 CREATE TABLE MEMBERSHIP (
     membership_id       NUMBER,
-    account_id          NUMBER  NOT NULL,
-    membership_type_id  NUMBER  NOT NULL,
-    accumulated_points  NUMBER  DEFAULT 0,
+    account_id          NUMBER      NOT NULL,
+    membership_type_id  VARCHAR(4)  NOT NULL,
+    accumulated_points  NUMBER      DEFAULT 0,
     PRIMARY KEY (membership_id),
     FOREIGN KEY (account_id)         REFERENCES ACCOUNT(account_id),
     FOREIGN KEY (membership_type_id) REFERENCES MEMBERSHIP_TYPE(membership_type_id)
