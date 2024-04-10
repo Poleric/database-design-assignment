07ab76a8608d30d5e45901fa413ac4a34ef1bd6d
Poleric <nixonleongkb@gmail.com>
2024-04-10 15:06:12 +0800

    update membership type

--- scripts/create/0createMembershipType.sql
+++ scripts/create/0createMembershipType.sql
@@ -1,8 +1,7 @@
 CREATE TABLE MEMBERSHIP_TYPE (
-    membership_type_id    NUMBER,
-    membership_type_name  VARCHAR(20)   NOT NULL,
-    membership_discount   NUMBER(3, 0)  DEFAULT 0,  -- whole number
-    membership_perks      VARCHAR(64),
-    PRIMARY KEY (membership_type_id),
-    CONSTRAINT check_discount_valid CHECK (membership_discount < 100 AND membership_discount > 0)
+    membership_type_id      NUMBER        NOT NULL,
+    membership_type_name    VARCHAR(20)   NOT NULL,
+    membership_description  VARCHAR(100),
+    membership_discount     NUMBER(3, 0)  DEFAULT 0  CHECK (membership_discount < 100 AND membership_discount > 0),  -- whole number
+    PRIMARY KEY (membership_type_id)
 );
