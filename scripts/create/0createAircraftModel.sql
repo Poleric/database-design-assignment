34891416430a8f0c0f5215a7fdd191b5ce84372d
Poleric <nixonleongkb@gmail.com>
2024-04-10 14:46:13 +0800

    remove aircraft type

--- scripts/create/0createAircraftModel.sql
+++ scripts/create/0createAircraftModel.sql
@@ -1,10 +1,8 @@
 CREATE TABLE AIRCRAFT_MODEL (
     aircraft_model_id   VARCHAR(4)   NOT NULL,  -- https://en.wikipedia.org/wiki/List_of_aircraft_type_designators
-    aircraft_type_id    VARCHAR(3)   NOT NULL,
     model_name          VARCHAR(20)  NOT NULL,
     model_manufacturer  VARCHAR(20)  NOT NULL,
     release_date        DATE         NOT NULL,
-    PRIMARY KEY (aircraft_model_id),
-    FOREIGN KEY (aircraft_type_id) REFERENCES AIRCRAFT_TYPE(aircraft_type_id)
+    PRIMARY KEY (aircraft_model_id)
 );
 
