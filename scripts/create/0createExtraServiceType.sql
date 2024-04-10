a12d1f894e5b86e4dd7f8e61dff261d792738758
Poleric <nixonleongkb@gmail.com>
2024-04-10 14:23:20 +0800

    remove service type

--- scripts/create/0createExtraServiceType.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE EXTRA_SERVICE_TYPE(
-    extra_service_type_id VARCHAR(20) NOT NULL,
-    extra_service_type_name VARCHAR(20) NOT NULL,
-    extra_service_type_description VARCHAR(200),
-    PRIMARY KEY (extra_service_type_id)
-)
