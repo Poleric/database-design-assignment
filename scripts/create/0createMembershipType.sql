41d78deddb254582665bb2f4f66d514c47e83314
Poleric <nixonleongkb@gmail.com>
2024-04-10 15:07:25 +0800

    make membership type id varchar(4)

--- scripts/create/0createMembershipType.sql
+++ scripts/create/0createMembershipType.sql
@@ -1,5 +1,5 @@
 CREATE TABLE MEMBERSHIP_TYPE (
-    membership_type_id      NUMBER        NOT NULL,
+    membership_type_id      VARCHAR(4)    NOT NULL,
     membership_type_name    VARCHAR(20)   NOT NULL,
     membership_description  VARCHAR(100),
     membership_discount     NUMBER(3, 0)  DEFAULT 0  CHECK (membership_discount < 100 AND membership_discount > 0),  -- whole number
