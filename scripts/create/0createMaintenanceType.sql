b535e2ec3884b04ff8bd33c39a12c964f694a196
Poleric <nixonleongkb@gmail.com>
2024-04-10 14:35:37 +0800

    change maintenance type id to varchar(4)

--- scripts/create/0createMaintenanceType.sql
+++ scripts/create/0createMaintenanceType.sql
@@ -1,5 +1,5 @@
 CREATE TABLE MAINTENANCE_TYPE(
-    maintenance_type_id NUMBER NOT NULL,
+    maintenance_type_id VARCHAR(4) NOT NULL,
     maintenance_type_name VARCHAR(20) NOT NULL,
     maintenance_type_description VARCHAR(100) NOT NULL,
     importance_level VARCHAR(20) NOT NULL,
--- scripts/create/3createMaintenanceLog.sql
+++ scripts/create/3createMaintenanceLog.sql
@@ -1,7 +1,7 @@
 CREATE TABLE MAINTENANCE_LOG(
     maintenance_log_id NUMBER NOT NULL,
     aircraft_id VARCHAR(10) NOT NULL,
-    maintenance_type_id NUMBER NOT NULL,
+    maintenance_type_id VARCHAR(4) NOT NULL,
     maintenance_date_time DATE NOT NULL,
     maintenance_description VARCHAR(200),
     PRIMARY KEY (maintenance_log_id),
