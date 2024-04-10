b0f2fa7c83599deba5629859f87f932253236c50
Poleric <nixonleongkb@gmail.com>
2024-04-10 15:08:37 +0800

    update seat class

--- scripts/create/0createSeatClass.sql
+++ scripts/create/0createSeatClass.sql
@@ -1,6 +1,6 @@
 CREATE TABLE SEAT_CLASS(
-    seat_class_id VARCHAR(20) NOT NULL,
-    class_name VARCHAR(20) NOT NULL,
-    class_priority NUMBER NOT NULL,
+    seat_class_id   VARCHAR(4)   NOT NULL,
+    class_name      VARCHAR(20)  NOT NULL,
+    class_priority  NUMBER       NOT NULL  CHECK (class_priority >= 0),
     PRIMARY KEY (seat_class_id)
-)
+);
