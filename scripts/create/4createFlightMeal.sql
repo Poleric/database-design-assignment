07716ba6359de857ab257a6386ee668c361236a6
Poleric <nixonleongkb@gmail.com>
2024-04-11 00:53:36 +0800

    create flight meal

--- scripts/create/4createFlightMeal.sql
+++ scripts/create/4createFlightMeal.sql
@@ -1,7 +1,6 @@
 CREATE TABLE FLIGHT_MEAL(
-    meal_id VARCHAR(10) NOT NULL,
-    flight_id VARCHAR(10) NOT NULL,
-    PRIMARY KEY(meal_id,flight_id),
-    FOREIGN KEY(meal_id) REFERENCES MEAL(meal_id),
-    FOREIGN KEY(flight_id) REFERENCES FLIGHT(flight_id)
-)
+    meal_id    VARCHAR(10) NOT NULL REFERENCES MEAL(meal_id),
+    flight_id  VARCHAR(10) NOT NULL REFERENCES FLIGHT(flight_id),
+    meal_for   VARCHAR(20) NOT NULL,
+    PRIMARY KEY(meal_id, flight_id)
+);
