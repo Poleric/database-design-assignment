0235f0d6d0c8ea41608db976692c60d35d46e653
Poleric <nixonleongkb@gmail.com>
2024-04-10 22:15:49 +0800

    update baggage payment

--- scripts/create/2createBaggagePayment.sql
+++ scripts/create/2createBaggagePayment.sql
@@ -1,7 +1,6 @@
 CREATE TABLE BAGGAGE_PAYMENT(
-    baggage_payment_id VARCHAR(10) NOT NULL,
-    transcation_id VARCHAR(10) NOT NULL,
-    payment_amount NUMBER,
+    baggage_payment_id  VARCHAR(10) NOT NULL,
+    transaction_id      VARCHAR(10) NOT NULL,
     PRIMARY KEY(baggage_payment_id),
     FOREIGN KEY(transaction_id) REFERENCES TRANSACTION(transaction_id)
-)
+);
