7e328cd455758408bce1eaa818445b247411d2d0
Poleric <nixonleongkb@gmail.com>
2024-04-11 00:42:38 +0800

    fix primary key

--- scripts/create/2createFlightHour.sql
+++ scripts/create/2createFlightHour.sql
@@ -1,5 +1,6 @@
 CREATE TABLE FLIGHT_HOUR (
-    pilot_id           NUMBER      PRIMARY KEY REFERENCES PILOT(pilot_id),
-    aircraft_model_id  VARCHAR(4)  PRIMARY KEY REFERENCES AIRCRAFT_MODEL(aircraft_model_id),
-    flight_hour        NUMBER
+    pilot_id           NUMBER      REFERENCES PILOT(pilot_id),
+    aircraft_model_id  VARCHAR(4)  REFERENCES AIRCRAFT_MODEL(aircraft_model_id),
+    flight_hour        NUMBER,
+    PRIMARY KEY (pilot_id, flight_hour)
 );
