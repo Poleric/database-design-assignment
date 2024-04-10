26c6a996ace6a8296253c727f9e677a88706ad26
Poleric <nixonleongkb@gmail.com>
2024-04-11 00:32:58 +0800

    create maintenance log

--- scripts/create/3createMaintenanceLog.sql
+++ scripts/create/3createMaintenanceLog.sql
@@ -1,10 +1,7 @@
 CREATE TABLE MAINTENANCE_LOG(
-    maintenance_log_id NUMBER NOT NULL,
-    aircraft_id VARCHAR(10) NOT NULL,
-    maintenance_type_id VARCHAR(4) NOT NULL,
-    maintenance_date_time DATE NOT NULL,
-    maintenance_description VARCHAR(200),
-    PRIMARY KEY (maintenance_log_id),
-    FOREIGN KEY (maintenance_type_id) REFERENCES MAINTENANCE_TYPE(maintenance_type_id),
-    FOREIGN KEY (aircraft_id) REFERENCES AIRCRAFT(aircraft_id)
-)
+    maintenance_log_id       NUMBER        PRIMARY KEY,
+    aircraft_id              VARCHAR(10)   NOT NULL REFERENCES AIRCRAFT(aircraft_id),
+    maintenance_type_id      VARCHAR(4)    NOT NULL REFERENCES MAINTENANCE_TYPE(maintenance_type_id),
+    maintenance_datetime     TIMESTAMP     NOT NULL,
+    maintenance_description  VARCHAR(100)  NOT NULL
+);
