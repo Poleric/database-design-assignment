5db64448f69768969b7d49d682a700f25c388eaa
Poleric <nixonleongkb@gmail.com>
2024-04-11 00:44:43 +0800

    create refund

--- scripts/create/3createRefund.sql
+++ scripts/create/3createRefund.sql
@@ -1,10 +1,6 @@
 CREATE TABLE REFUND(
-    refund_id VARCHAR(10) NOT NULL,
-    transaction_id NUMBER NOT NULL,
-    payment_id VARCHAR(10) NOT NULL,
-    refund_amount NUMBER(7,2),
-    reason VARCHAR(200),
-    PRIMARY KEY (refund_id),
-    FOREIGN KEY (transaction_id) REFERENCES TRANSACTION(transaction_id),
-    FOREIGN KEY (payment_id) REFERENCES TICKET_PAYMENT(payment_id)
-)
+    refund_id       VARCHAR(10)   PRIMARY KEY,
+    transaction_id  VARCHAR(10)   NOT NULL REFERENCES TRANSACTION(transaction_id),
+    payment_id      VARCHAR(10)   NOT NULL REFERENCES PAYMENT(payment_id),
+    reason          VARCHAR(100)  NOT NULL
+);
