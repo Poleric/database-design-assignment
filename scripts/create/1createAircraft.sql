7aea1c010f5adae0618cc45acac14c2629005bb7
Poleric <nixonleongkb@gmail.com>
2024-04-11 15:03:59 +0800

    change aircraft id varchar length

--- scripts/create/1createAircraft.sql
+++ scripts/create/1createAircraft.sql
@@ -1,6 +1,6 @@
 CREATE TABLE AIRCRAFT (
-    aircraft_id        VARCHAR(10)  PRIMARY KEY,
-    aircraft_model_id  VARCHAR(4)   NOT NULL UNIQUE REFERENCES AIRCRAFT_MODEL(aircraft_model_id),
-    capacity           NUMBER(3)    NOT NULL,
-    purchase_date      DATE         NOT NULL
+    aircraft_id        VARCHAR(6)  PRIMARY KEY,
+    aircraft_model_id  VARCHAR(4)  NOT NULL UNIQUE REFERENCES AIRCRAFT_MODEL(aircraft_model_id),
+    capacity           NUMBER(3)   NOT NULL,
+    purchase_date      DATE        NOT NULL
 );
--- scripts/create/3createMaintenanceLog.sql
+++ scripts/create/3createMaintenanceLog.sql
@@ -1,6 +1,6 @@
 CREATE TABLE MAINTENANCE_LOG(
     maintenance_log_id       NUMBER        PRIMARY KEY,
-    aircraft_id              VARCHAR(10)   NOT NULL REFERENCES AIRCRAFT(aircraft_id),
+    aircraft_id              VARCHAR(6)   NOT NULL REFERENCES AIRCRAFT(aircraft_id),
     maintenance_type_id      VARCHAR(4)    NOT NULL REFERENCES MAINTENANCE_TYPE(maintenance_type_id),
     maintenance_datetime     TIMESTAMP     NOT NULL,
     maintenance_description  VARCHAR(100)  NOT NULL
