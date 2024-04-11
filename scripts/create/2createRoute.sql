31a141f0048be27935b3ed85c3f09660b966d9bb
Poleric <nixonleongkb@gmail.com>
2024-04-11 20:51:12 +0800

    route id is the flight id

--- scripts/create/2createRoute.sql
+++ scripts/create/2createRoute.sql
@@ -1,5 +1,5 @@
 CREATE TABLE ROUTE (
-    route_id           VARCHAR(7) PRIMARY KEY,
+    route_id           VARCHAR(7) PRIMARY KEY,  -- https://en.wikipedia.org/wiki/Flight_number
     from_airport_id    VARCHAR(3) NOT NULL REFERENCES AIRPORT(airport_id),
     to_airport_id      VARCHAR(3) NOT NULL REFERENCES AIRPORT(airport_id),
     route_description  VARCHAR(100)
