33ddafe567e705a1dbfda6e874dbbe0b7fb1d88f
Poleric <nixonleongkb@gmail.com>
2024-04-11 23:14:02 +0800

    add reference

--- scripts/create/0createBaggageType.sql
+++ scripts/create/0createBaggageType.sql
@@ -1,3 +1,4 @@
+-- https://www.kiwi.com/en/help/baggage-allowance-173/article/types-of-baggage-246/
 CREATE TABLE BAGGAGE_TYPE(
     baggage_type_id           VARCHAR(3)    PRIMARY KEY,
     baggage_type_name         VARCHAR(20)   NOT NULL UNIQUE,
