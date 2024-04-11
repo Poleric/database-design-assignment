24d0a7bba8137299f5795759b4b43b2f69289cb8
Poleric <nixonleongkb@gmail.com>
2024-04-12 00:01:33 +0800

    change description length

--- scripts/create/0createMeal.sql
+++ scripts/create/0createMeal.sql
@@ -1,6 +1,6 @@
 CREATE TABLE MEAL(
     meal_id           NUMBER       PRIMARY KEY,
     meal_name         VARCHAR(30)  NOT NULL,
-    meal_description  VARCHAR(100),
+    meal_description  VARCHAR(200),
     supplier          VARCHAR(30)  NOT NULL
 );
