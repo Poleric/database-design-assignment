c088d15666dd4560a5850b4f1941b122e02a414d
HongJunChea <cheahongjun@gmail.com>
2024-04-12 00:11:13 +0800

    CREWROLE

---
+++ scripts/insert/0insertCrewRole.sql
@@ -0,0 +1,8 @@
+INSERT ALL
+    INTO CREW_ROLE (crew_role_id, role_name) VALUES ('CPTN','Captain')
+    INTO CREW_ROLE (crew_role_id, role_name) VALUES ('FSOF','First officer')
+    INTO CREW_ROLE (crew_role_id, role_name) VALUES ('SCOF','Second officer')
+    INTO CREW_ROLE (crew_role_id, role_name) VALUES ('FLEN','Flight Engineer')
+    INTO CREW_ROLE (crew_role_id, role_name) VALUES ('PURS','Purser')
+    INTO CREW_ROLE (crew_role_id, role_name) VALUES ('FLAT','Flight attendant')
+SELECT 1 FROM DUAL;
