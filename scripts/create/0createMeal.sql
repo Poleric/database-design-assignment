7e259cff5977f25b6951e81eff892376c17e94c3
Poleric <nixonleongkb@gmail.com>
2024-04-11 23:59:49 +0800

    change supplier length

--- scripts/create/0createMeal.sql
+++ scripts/create/0createMeal.sql
@@ -2,5 +2,5 @@ CREATE TABLE MEAL(
     meal_id           NUMBER       PRIMARY KEY,
     meal_name         VARCHAR(30)  NOT NULL,
     meal_description  VARCHAR(100),
-    supplier          VARCHAR(20)  NOT NULL
+    supplier          VARCHAR(30)  NOT NULL
 );
