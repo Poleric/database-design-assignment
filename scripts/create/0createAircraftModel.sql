b2ea70fb9dd3fa55a26b8a3e1a1909819ec5ce50
Poleric <nixonleongkb@gmail.com>
2024-04-12 01:06:23 +0800

    change name and manufacturer length

--- scripts/create/0createAircraftModel.sql
+++ scripts/create/0createAircraftModel.sql
@@ -1,7 +1,7 @@
 CREATE TABLE AIRCRAFT_MODEL (
     aircraft_model_id   VARCHAR(4)   PRIMARY KEY ,  -- https://en.wikipedia.org/wiki/List_of_aircraft_type_designators
-    model_name          VARCHAR(20)  NOT NULL UNIQUE,
-    model_manufacturer  VARCHAR(20)  NOT NULL,
+    model_name          VARCHAR(30)  NOT NULL UNIQUE,
+    model_manufacturer  VARCHAR(30)  NOT NULL,
     number_of_seat      NUMBER       NOT NULL CHECK (number_of_seat > 0),
     release_date        DATE         NOT NULL
 );
