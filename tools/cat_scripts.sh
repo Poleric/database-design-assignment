c88a91fd97d0618edd4aa2ca4c542f55e0ac442c
Poleric <nixonleongkb@gmail.com>
2024-04-11 14:30:38 +0800

    fix missing .sql

--- tools/cat_scripts.sh
+++ tools/cat_scripts.sh
@@ -6,10 +6,10 @@ cd "$(dirname "$0")" || return
 cd ../scripts/ || return
 
 # remove old
-rm create_all.sql insert_all.sql run.sql 2> /dev/null
+rm create_all.sql create_trigger_all insert_all.sql run.sql 2> /dev/null
 
 awk '{print $0}' ./create/*.sql > create_all.sql
 awk '{print $0}' ./trigger/*.sql > create_trigger_all.sql
 awk '{print $0}' ./insert/*.sql > insert_all.sql
 
-awk '{print $0}' create_all.sql create_trigger_all insert_all.sql > run.sql
+awk '{print $0}' create_all.sql create_trigger_all.sql insert_all.sql > run.sql
