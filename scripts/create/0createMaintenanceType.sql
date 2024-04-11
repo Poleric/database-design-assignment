72fac3134cfa3a9f8f343381f57e1dd443206e2d
Poleric <nixonleongkb@gmail.com>
2024-04-12 02:44:54 +0800

    increase varchar lengths

--- scripts/create/0createMaintenanceType.sql
+++ scripts/create/0createMaintenanceType.sql
@@ -1,6 +1,6 @@
 -- https://www.quora.com/What-are-the-types-of-aircraft-maintenance  https://en.wikipedia.org/wiki/Aircraft_maintenance_checks
 CREATE TABLE MAINTENANCE_TYPE(
     maintenance_type_id           VARCHAR(4)    PRIMARY KEY ,
-    maintenance_type_name         VARCHAR(20)   NOT NULL UNIQUE,
-    maintenance_type_description  VARCHAR(100)  NOT NULL
+    maintenance_type_name         VARCHAR(40)   NOT NULL UNIQUE,
+    maintenance_type_description  VARCHAR(200)  NOT NULL
 );
