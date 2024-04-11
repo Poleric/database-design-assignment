b559893e8f6c95fe2aa505def51462feab353306
HongJunChea <cheahongjun@gmail.com>
2024-04-11 23:46:00 +0800

    SEATCLASS AND RENAME

---
+++ scripts/insert/0insertSeatClass.sql
@@ -0,0 +1,5 @@
+INSERT ALL
+    INTO SEAT_CLASS (seat_class_id, class_name, class_priority) VALUES ('ECON','Economy Class','LOW')
+    INTO SEAT_CLASS (seat_class_id, class_name, class_priority) VALUES ('BUSI','Business Class','MEDIUM')
+    INTO SEAT_CLASS (seat_class_id, class_name, class_priority) VALUES ('FIRS','First Class','HIGH')
+SELECT 1 FROM DUAL;
