6034f7187e3880ea8e2390547353659775949aef
Poleric <nixonleongkb@gmail.com>
2024-04-11 22:19:00 +0800

    change flight meal id to number

--- scripts/create/0createMeal.sql
+++ scripts/create/0createMeal.sql
@@ -1,6 +1,6 @@
 CREATE TABLE MEAL(
-    meal_id           VARCHAR(10) PRIMARY KEY,
-    meal_name         VARCHAR(20) NOT NULL,
+    meal_id           NUMBER       PRIMARY KEY,
+    meal_name         VARCHAR(20)  NOT NULL,
     meal_description  VARCHAR(100),
-    supplier          VARCHAR(20) NOT NULL
+    supplier          VARCHAR(20)  NOT NULL
 );
--- scripts/create/4createFlightMeal.sql
+++ scripts/create/4createFlightMeal.sql
@@ -1,5 +1,5 @@
 CREATE TABLE FLIGHT_MEAL(
-    meal_id    VARCHAR(10)  NOT NULL REFERENCES MEAL(meal_id),
+    meal_id    NUMBER       NOT NULL REFERENCES MEAL(meal_id),
     flight_id  NUMBER       NOT NULL REFERENCES FLIGHT(flight_id),
     meal_for   VARCHAR(20)  NOT NULL,
     PRIMARY KEY(meal_id, flight_id)
