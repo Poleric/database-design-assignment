4b52c2fe6c07702015d1b478991f3d104b113b29
Poleric <nixonleongkb@gmail.com>
2024-04-11 15:05:45 +0800

    change flight attendant id length

--- scripts/create/1createFlightAttendant.sql
+++ scripts/create/1createFlightAttendant.sql
@@ -1,4 +1,4 @@
 CREATE TABLE FLIGHT_ATTENDANT (
-    flight_attendant_id  VARCHAR(10)  PRIMARY KEY,
+    flight_attendant_id  VARCHAR(7)  PRIMARY KEY,
     employee_id          NUMBER       REFERENCES EMPLOYEE (employee_id)
 );
