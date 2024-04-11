d8780e67a5d0f8c441dd05e132e0ed0ef6bf111f
Poleric <nixonleongkb@gmail.com>
2024-04-12 02:28:00 +0800

    change baggage id and description to fit length

--- scripts/insert/0insertBaggageType.sql
+++ scripts/insert/0insertBaggageType.sql
@@ -1,6 +1,6 @@
 INSERT ALL
-    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CBGI','Check baggage in','Before your flight, you have to bring checked baggage to the airline’s check-in desk. The airline staff will weigh it and send it to the aircraft.')
-    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CBBG','Cabin baggage','Cabin baggage is also known as carry-on or hand luggage, and can be a backpack or a small suitcase.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CHK', 'Check in baggage', 'Checked baggage at the airline’s check-in desk. Weighed before sending to the flight.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CO', 'Carry-on baggage', 'Cabin baggage is also known as carry-on or hand luggage, and can be a backpack or a small suitcase.')
 SELECT 1 FROM DUAL;
 
 
