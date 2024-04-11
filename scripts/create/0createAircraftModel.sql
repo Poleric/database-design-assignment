e784a04e72d5ae302089c645471feba9d2563c54
Poleric <nixonleongkb@gmail.com>
2024-04-12 00:25:17 +0800

    add number of seat to aircraft model

--- scripts/create/0createAircraftModel.sql
+++ scripts/create/0createAircraftModel.sql
@@ -2,5 +2,6 @@ CREATE TABLE AIRCRAFT_MODEL (
     aircraft_model_id   VARCHAR(4)   PRIMARY KEY ,  -- https://en.wikipedia.org/wiki/List_of_aircraft_type_designators
     model_name          VARCHAR(20)  NOT NULL UNIQUE,
     model_manufacturer  VARCHAR(20)  NOT NULL,
+    number_of_seat      NUMBER       NOT NULL CHECK (number_of_seat > 0)
     release_date        DATE         NOT NULL
 );
