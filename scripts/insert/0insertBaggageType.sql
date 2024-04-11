61d7b2f645254bdda523ecb25992bb82699e805d
HongJunChea <cheahongjun@gmail.com>
2024-04-11 23:16:28 +0800

    BAGGAGE TYPE

---
+++ scripts/insert/0insertBaggageType.sql
@@ -0,0 +1,6 @@
+INSERT ALL
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CBGI','Check baggage in','Before your flight, you have to bring checked baggage to the airline’s check-in desk. The airline staff will weigh it and send it to the aircraft.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CBBG','Cabin baggage','Cabin baggage is also known as carry-on or hand luggage, and can be a backpack or a small suitcase.')
+SELECT 1 FROM DUAL;
+
+
