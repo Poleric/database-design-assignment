df5b1ec1c2d3dda1d27634cacbea691f5507eca6
Poleric <nixonleongkb@gmail.com>
2024-04-11 15:57:50 +0800

    rename col

--- scripts/create/2createRestLog.sql
+++ scripts/create/2createRestLog.sql
@@ -1,5 +1,5 @@
 CREATE TABLE REST_LOG (
-    rest_id         NUMBER       PRIMARY KEY,
+    rest_log_id         NUMBER       PRIMARY KEY,
     pilot_id        VARCHAR(6)  NOT NULL REFERENCES PILOT (pilot_id),
     start_datetime  TIMESTAMP    NOT NULL,
     end_datetime    TIMESTAMP    NOT NULL
