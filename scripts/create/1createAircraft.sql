c14f519f7b6bb91f6b76a31f616e85d990944658
Poleric <nixonleongkb@gmail.com>
2024-04-11 20:48:10 +0800

    add reference

--- scripts/create/1createAircraft.sql
+++ scripts/create/1createAircraft.sql
@@ -1,5 +1,5 @@
 CREATE TABLE AIRCRAFT (
-    aircraft_id        VARCHAR(6)  PRIMARY KEY,
+    aircraft_id        VARCHAR(6)  PRIMARY KEY,  -- https://en.wikipedia.org/wiki/Aircraft_registration
     aircraft_model_id  VARCHAR(4)  NOT NULL UNIQUE REFERENCES AIRCRAFT_MODEL(aircraft_model_id),
     capacity           NUMBER(3)   NOT NULL,
     purchase_date      DATE        NOT NULL
