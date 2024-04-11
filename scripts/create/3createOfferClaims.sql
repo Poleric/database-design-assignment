8b355279481f145a7733c4f7d74fe3282903e522
Poleric <nixonleongkb@gmail.com>
2024-04-11 15:19:06 +0800

    change offer id to int

--- scripts/create/3createOfferClaims.sql
+++ scripts/create/3createOfferClaims.sql
@@ -1,6 +1,6 @@
 CREATE TABLE OFFER_CLAIMS(
-    payment_id        VARCHAR(10) REFERENCES PAYMENT(payment_id),
-    offer_id          VARCHAR(10) REFERENCES OFFER(offer_id),
-    datetime_claimed  TIMESTAMP NOT NULL,
+    payment_id        VARCHAR(10)  REFERENCES PAYMENT(payment_id),
+    offer_id          NUMBER       REFERENCES OFFER(offer_id),
+    datetime_claimed  TIMESTAMP    NOT NULL,
     PRIMARY KEY (payment_id, offer_id)
 );
