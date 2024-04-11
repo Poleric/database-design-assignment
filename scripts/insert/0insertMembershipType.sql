c326599ec8294752722cbcf64d8713fc8dcdb35e
HongJunChea <cheahongjun@gmail.com>
2024-04-11 22:47:06 +0800

    MEMBERSHIP TYPE

---
+++ scripts/insert/0insertMembershipType.sql
@@ -0,0 +1,4 @@
+INSERT ALL
+    INTO MEMBERSHIP_TYPE (membership_type_id, membership_type_name, membership_description, membership_discount) VALUES ('GOLD','GOLD','DISCOUNT','15')
+    INTO MEMBERSHIP_TYPE (membership_type_id, membership_type_name, membership_description, membership_discount) VALUES ('SILV','SILVER','DISCOUNT','10')
+    INTO MEMBERSHIP_TYPE (membership_type_id, membership_type_name, membership_description, membership_discount) VALUES ('BRONZ','BRONZE','DISCOUNT','5')
