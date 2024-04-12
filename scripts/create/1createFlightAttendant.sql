a25bed3fd293f2db4db703eae7c84cd2b8ec557b
Poleric <nixonleongkb@gmail.com>
2024-04-12 12:06:52 +0800

    add unique to employee_id

--- scripts/create/1createFlightAttendant.sql
+++ scripts/create/1createFlightAttendant.sql
@@ -1,4 +1,4 @@
 CREATE TABLE FLIGHT_ATTENDANT (
     flight_attendant_id  VARCHAR(7)  PRIMARY KEY,
-    employee_id          NUMBER       REFERENCES EMPLOYEE (employee_id)
+    employee_id          NUMBER      UNIQUE REFERENCES EMPLOYEE (employee_id)
 );
