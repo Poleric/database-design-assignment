2dd6d6b9e9fe6161414ff7257d90cd919aa0236d
Poleric <nixonleongkb@gmail.com>
2024-04-13 10:13:55 +0800

    remove total flight hour field

--- scripts/create/1createPilot.sql
+++ scripts/create/1createPilot.sql
@@ -1,6 +1,5 @@
 CREATE TABLE PILOT (
     pilot_id              VARCHAR(6)   PRIMARY KEY,
     employee_id           NUMBER       NOT NULL UNIQUE REFERENCES EMPLOYEE(employee_id),
-    pilot_license_number  VARCHAR(10)  NOT NULL,
-    total_flight_hour     NUMBER       DEFAULT 0
+    pilot_license_number  VARCHAR(10)  NOT NULL
 );
