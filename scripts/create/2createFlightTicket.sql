75f2db09de20124739058685dcd2861b54051e60
Poleric <nixonleongkb@gmail.com>
2024-04-17 12:02:19 +0800

    field name date -> datetime

--- scripts/create/2createFlightTicket.sql
+++ scripts/create/2createFlightTicket.sql
@@ -2,7 +2,7 @@ CREATE TABLE FLIGHT_TICKET(
     flight_ticket_id    VARCHAR(13)  PRIMARY KEY,
     account_id          NUMBER       NOT NULL  REFERENCES ACCOUNT(account_id),
     payment_id          VARCHAR(10)  NOT NULL  REFERENCES PAYMENT(payment_id),
-    datetime_placed     TIMESTAMP    NOT NULL,
+    created_at          TIMESTAMP    NOT NULL,
     boarding_group      NUMBER       NOT NULL  CHECK (boarding_group > 0),
     ticket_price        NUMBER(7,2)  NOT NULL,
     ticket_status       VARCHAR(9)   NOT NULL  CHECK (ticket_status IN ('Completed', 'Paid', 'Cancelled') ),
