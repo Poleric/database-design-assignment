40ae3dcd82fa481c30b4f588653d1e9969601525
Poleric <nixonleongkb@gmail.com>
2024-04-17 11:47:20 +0800

    do payment

--- scripts/create/1createPayment.sql
+++ scripts/create/1createPayment.sql
@@ -1,5 +1,8 @@
 CREATE TABLE PAYMENT(
-    payment_id      VARCHAR(10) PRIMARY KEY,
-    transaction_id  VARCHAR(10) NOT NULL REFERENCES TRANSACTION(transaction_id),
-    account_id      NUMBER      NOT NULL REFERENCES ACCOUNT(account_id)
+    payment_id         VARCHAR(10)  PRIMARY KEY,
+    payment_method_id  VARCHAR(10)  NOT NULL REFERENCES PAYMENT_METHOD(payment_method_id),
+    account_id         NUMBER       NOT NULL REFERENCES ACCOUNT(account_id),
+    created_at         TIMESTAMP    NOT NULL,
+    amount             NUMBER       NOT NULL,
+    reference          VARCHAR(200)
 );
