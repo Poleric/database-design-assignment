5d6543c0ef141becd6de61c5236494863f818ae3
Poleric <nixonleongkb@gmail.com>
2024-04-17 17:41:50 +0800

    increase id length

--- scripts/create/2createSeat.sql
+++ scripts/create/2createSeat.sql
@@ -1,5 +1,5 @@
 CREATE TABLE SEAT(
-    seat_id        VARCHAR(10)  PRIMARY KEY,
+    seat_id        VARCHAR(12)  PRIMARY KEY,
     aircraft_id    VARCHAR(6)   NOT NULL REFERENCES AIRCRAFT (aircraft_id),
     seat_class_id  VARCHAR(10)  NOT NULL REFERENCES SEAT_CLASS (seat_class_id),
     location       VARCHAR(6)   NOT NULL
