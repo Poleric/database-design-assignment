1e29f002ecc1c713a4ccf967d9ca30cab515edaa
Poleric <nixonleongkb@gmail.com>
2024-04-17 11:49:44 +0800

    fix refund

--- scripts/create/2createRefund.sql
+++ scripts/create/2createRefund.sql
@@ -1,5 +1,6 @@
 CREATE TABLE REFUND(
-    refund_id       VARCHAR(10)   PRIMARY KEY,
-    payment_id      VARCHAR(10)   NOT NULL REFERENCES PAYMENT(payment_id),
-    reason          VARCHAR(100)  NOT NULL
+    refund_id   VARCHAR(10)   PRIMARY KEY,
+    payment_id  VARCHAR(10)   NOT NULL REFERENCES PAYMENT(payment_id),
+    created_at  TIMESTAMP     NOT NULL,
+    reason      VARCHAR(100)  NOT NULL
 );
