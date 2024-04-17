01ac2e9bcec47c9076b7fdf182e5e37e0438ddaf
Poleric <nixonleongkb@gmail.com>
2024-04-17 12:01:17 +0800

    add clock in and clock out to crew schedule

--- scripts/create/4createFlightCrewSchedule.sql
+++ scripts/create/4createFlightCrewSchedule.sql
@@ -1,6 +1,8 @@
 CREATE TABLE FLIGHT_CREW_SCHEDULE(
-    employee_id   NUMBER      NOT NULL REFERENCES EMPLOYEE(employee_id),
-    flight_id     NUMBER      NOT NULL REFERENCES FLIGHT(flight_id),
-    crew_role_id  VARCHAR(4)  NOT NULL REFERENCES CREW_ROLE(crew_role_id),
+    employee_id     NUMBER      NOT NULL REFERENCES EMPLOYEE(employee_id),
+    flight_id       NUMBER      NOT NULL REFERENCES FLIGHT(flight_id),
+    crew_role_id    VARCHAR(4)  NOT NULL REFERENCES CREW_ROLE(crew_role_id),
+    clock_in_time   TIMESTAMP,
+    clock_out_time  TIMESTAMP,
     PRIMARY KEY(employee_id, flight_id)
 );
