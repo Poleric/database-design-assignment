72f07fd0143915c794462e401add13f1a520b671
Poleric <nixonleongkb@gmail.com>
2024-04-17 11:49:01 +0800

    do refund table

--- scripts/create/2createRefund.sql
+++ scripts/create/2createRefund.sql
@@ -1,6 +1,5 @@
 CREATE TABLE REFUND(
     refund_id       VARCHAR(10)   PRIMARY KEY,
-    transaction_id  VARCHAR(10)   NOT NULL REFERENCES TRANSACTION(transaction_id),
     payment_id      VARCHAR(10)   NOT NULL REFERENCES PAYMENT(payment_id),
     reason          VARCHAR(100)  NOT NULL
 );
