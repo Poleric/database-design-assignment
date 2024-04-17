e0f96970f3b261a29ed97e60e9dcaafb58f61479
Poleric <nixonleongkb@gmail.com>
2024-04-17 11:59:47 +0800

    update crew role

--- scripts/create/0createCrewRole.sql
+++ scripts/create/0createCrewRole.sql
@@ -1,4 +1,5 @@
 CREATE TABLE CREW_ROLE(
-    crew_role_id  VARCHAR(4)   PRIMARY KEY ,
-    role_name     VARCHAR(20)  NOT NULL UNIQUE
+    crew_role_id      VARCHAR(4)   PRIMARY KEY ,
+    role_name         VARCHAR(20)  NOT NULL UNIQUE,
+    role_description  VARCHAR(200)
 );
