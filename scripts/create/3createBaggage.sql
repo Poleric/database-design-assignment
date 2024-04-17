d3d0e9f6b4a9b6badb897671468cc6de6beb4ae3
Poleric <nixonleongkb@gmail.com>
2024-04-17 11:52:34 +0800

    add not null checks

--- scripts/create/3createBaggage.sql
+++ scripts/create/3createBaggage.sql
@@ -3,7 +3,7 @@ CREATE TABLE BAGGAGE(
     baggage_type_id       VARCHAR(10)  NOT NULL REFERENCES BAGGAGE_TYPE(baggage_type_id),
     payment_id            VARCHAR(10)  NOT NULL REFERENCES PAYMENT(payment_id),
     flight_ticket_id      VARCHAR(13)  NOT NULL REFERENCES FLIGHT_TICKET(flight_ticket_id),
-    baggage_weight_in_kg  NUMBER,
-    price                 NUMBER,
+    baggage_weight_in_kg  NUMBER       NOT NULL,
+    price                 NUMBER       NOT NULL,
     additional_info       VARCHAR(100)
 );
