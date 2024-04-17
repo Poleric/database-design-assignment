04771303b8038f1c9b74aba57cfb734e72055dd0
Poleric <nixonleongkb@gmail.com>
2024-04-17 12:21:35 +0800

    rename to paymentmethod

---
+++ scripts/insert/0insertPaymentMethod.sql
@@ -0,0 +1,7 @@
+INSERT ALL
+    INTO PAYMENT_METHOD (payment_method_id, method_name, description) VALUES ('CRCD',' Credit Card Payment','Passengers can pay for their flight tickets using a credit card.')
+    INTO PAYMENT_METHOD (payment_method_id, method_name, description) VALUES ('DBCD',' Debit Card Payment','Passengers can use a debit card to purchase their flight tickets.')
+    INTO PAYMENT_METHOD (payment_method_id, method_name, description) VALUES ('PYPL',' PayPal','PayPal is an online payment platform that allows passengers to make secure transactions using their PayPal account.')
+    INTO PAYMENT_METHOD (payment_method_id, method_name, description) VALUES ('BANK','Bank Transfer','Passengers can initiate a bank transfer to pay for their flight tickets.')
+    INTO PAYMENT_METHOD (payment_method_id, method_name, description) VALUES ('CASH','Cash Payment','Passengers can pay for their flight tickets with cash at the airline ticket counter or designated payment location.')
+SELECT 1 FROM DUAL;
