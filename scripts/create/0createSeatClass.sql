2947581a3c7ec63cfa3ca74a52c13199df1f01d2
Poleric <nixonleongkb@gmail.com>
2024-04-17 17:39:24 +0800

    seat class "priority" -> "seat_desc"

--- scripts/create/0createSeatClass.sql
+++ scripts/create/0createSeatClass.sql
@@ -1,5 +1,5 @@
 CREATE TABLE SEAT_CLASS(
-    seat_class_id   VARCHAR(4)   PRIMARY KEY,
-    class_name      VARCHAR(20)  NOT NULL UNIQUE,
-    class_priority  VARCHAR(6)   NOT NULL
+    seat_class_id      VARCHAR(4)   PRIMARY KEY,
+    class_name         VARCHAR(20)  NOT NULL UNIQUE,
+    class_description  VARCHAR(100)
 );
