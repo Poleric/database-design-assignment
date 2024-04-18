6da3b08273f0797d21db68b5e814422adbd0ec29
Poleric <nixonleongkb@gmail.com>
2024-04-18 20:39:41 +0800

    make airport name non unique

--- scripts/create/1createAirport.sql
+++ scripts/create/1createAirport.sql
@@ -1,5 +1,5 @@
 CREATE TABLE AIRPORT (
     airport_id        VARCHAR(3)   PRIMARY KEY ,  -- https://en.wikipedia.org/wiki/IATA_airport_code
-    airport_fullname  VARCHAR(64)  NOT NULL UNIQUE,
+    airport_fullname  VARCHAR(64)  NOT NULL,
     country_id        VARCHAR(2)   NOT NULL REFERENCES COUNTRY(country_id)
 );
