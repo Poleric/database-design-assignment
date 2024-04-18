c3783c8602bbd1aca5be22f539f08967e6575c79
Poleric <nixonleongkb@gmail.com>
2024-04-18 20:32:47 +0800

    fix regex

--- scripts/create/0createAccount.sql
+++ scripts/create/0createAccount.sql
@@ -3,7 +3,7 @@ CREATE TABLE ACCOUNT (
     username         VARCHAR(255)  NOT NULL  UNIQUE,
     hashed_password  VARCHAR(60)   NOT NULL,
     joined_datetime  TIMESTAMP     NOT NULL,
-    email_address    VARCHAR(254)  NOT NULL  UNIQUE  CHECK (REGEXP_LIKE(email_address,'^(?:[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$')),  -- http://emailregex.com/
+    email_address    VARCHAR(254)  NOT NULL  UNIQUE  CHECK (REGEXP_LIKE(email_address,'^[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:)\])$')),  -- http://emailregex.com/
     phone_number     VARCHAR(20)             UNIQUE,
     gender           CHAR(1)                         CHECK (gender in ('M', 'F', NULL))
 );
