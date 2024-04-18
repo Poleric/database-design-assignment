98f59665dfd921dcfadf306e0965f94158836e32
Poleric <nixonleongkb@gmail.com>
2024-04-19 00:10:44 +0800

    change not null to default

--- scripts/create/3createFlight.sql
+++ scripts/create/3createFlight.sql
@@ -4,5 +4,5 @@ CREATE TABLE FLIGHT (
     aircraft_id           VARCHAR(6)  NOT NULL REFERENCES AIRCRAFT(aircraft_id),
     departure_datetime    TIMESTAMP   NOT NULL,
     est_duration_in_hour  NUMBER      NOT NULL,
-    is_codeshare          NUMBER(1)   NOT NULL CHECK (is_codeshare IN (0, 1) )  -- boolean
+    is_codeshare          NUMBER(1)   DEFAULT 0 CHECK (is_codeshare IN (0, 1) )  -- boolean
 );
