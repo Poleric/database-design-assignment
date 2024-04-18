19c4a115b131157f19c1dc237525c554aa0b5e97
Poleric <nixonleongkb@gmail.com>
2024-04-18 20:34:39 +0800

    increase desc varchar length

--- scripts/create/0createSeatClass.sql
+++ scripts/create/0createSeatClass.sql
@@ -1,5 +1,5 @@
 CREATE TABLE SEAT_CLASS(
     seat_class_id      VARCHAR(4)   PRIMARY KEY,
     class_name         VARCHAR(20)  NOT NULL UNIQUE,
-    class_description  VARCHAR(100)
+    class_description  VARCHAR(200)
 );
