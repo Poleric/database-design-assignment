8d09826aa7baf9d51ef53e6ce9d79e9cfa908130
Poleric <nixonleongkb@gmail.com>
2024-04-18 22:07:41 +0800

    add distance to route

--- scripts/create/2createRoute.sql
+++ scripts/create/2createRoute.sql
@@ -2,5 +2,6 @@ CREATE TABLE ROUTE (
     route_id           VARCHAR(7) PRIMARY KEY,  -- https://en.wikipedia.org/wiki/Flight_number
     from_airport_id    VARCHAR(3) NOT NULL REFERENCES AIRPORT(airport_id),
     to_airport_id      VARCHAR(3) NOT NULL REFERENCES AIRPORT(airport_id),
+    distance_in_km     NUMBER     NOT NULL,
     route_description  VARCHAR(100)
 );
