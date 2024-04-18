2eddcaa3129b3d2035e16065b6bb4a860ccccde4
Poleric <nixonleongkb@gmail.com>
2024-04-18 18:30:29 +0800

    change to description

--- scripts/insert/0insertSeatClass.sql
+++ scripts/insert/0insertSeatClass.sql
@@ -1,5 +1,5 @@
 INSERT ALL
-    INTO SEAT_CLASS (seat_class_id, class_name, class_priority) VALUES ('ECON','Economy Class','LOW')
-    INTO SEAT_CLASS (seat_class_id, class_name, class_priority) VALUES ('BUSI','Business Class','MEDIUM')
-    INTO SEAT_CLASS (seat_class_id, class_name, class_priority) VALUES ('FIRS','First Class','HIGH')
+    INTO SEAT_CLASS (seat_class_id, class_name, class_description) VALUES ('ECON', 'Economy Class', 'Cheap seating')
+    INTO SEAT_CLASS (seat_class_id, class_name, class_description) VALUES ('BUSI', 'Business Class', 'More refined experience. Includes on-flight meal and beverage.')
+    INTO SEAT_CLASS (seat_class_id, class_name, class_description) VALUES ('FIRS', 'First Class', 'Highest class of seating with large seating and luxurious meals. Comes with access to in-flight bar and lounge.')
 SELECT 1 FROM DUAL;
