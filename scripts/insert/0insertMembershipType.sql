ee35e221c892fb65f706d7c4769ccb7fabc0e75d
Poleric <nixonleongkb@gmail.com>
2024-04-18 23:44:09 +0800

    add description

--- scripts/insert/0insertMembershipType.sql
+++ scripts/insert/0insertMembershipType.sql
@@ -1,5 +1,5 @@
 INSERT ALL
-    INTO MEMBERSHIP_TYPE (membership_type_id, membership_type_name, membership_description, membership_discount) VALUES ('GOLD', 'GOLD', 'DISCOUNT', 15)
-    INTO MEMBERSHIP_TYPE (membership_type_id, membership_type_name, membership_description, membership_discount) VALUES ('SILV', 'SILVER', 'DISCOUNT', 10)
-    INTO MEMBERSHIP_TYPE (membership_type_id, membership_type_name, membership_description, membership_discount) VALUES ('BRNZ', 'BRONZE', 'DISCOUNT', 5)
+    INTO MEMBERSHIP_TYPE (membership_type_id, membership_type_name, membership_description, membership_discount) VALUES ('GOLD', 'Gold', 'Gold Membership with 15% discount each flights.', 15)
+    INTO MEMBERSHIP_TYPE (membership_type_id, membership_type_name, membership_description, membership_discount) VALUES ('SILV', 'Silver', 'Silver Membership with 10% discount each flights.', 10)
+    INTO MEMBERSHIP_TYPE (membership_type_id, membership_type_name, membership_description, membership_discount) VALUES ('BRNZ', 'Bronze', 'Bronze Membership with 5% discount each flights.', 5)
 SELECT 1 FROM DUAL;
