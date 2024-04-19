466ad188b44cd16e8025bdb11310e9fb6a05d143
HongJunChea <cheahongjun@gmail.com>
2024-04-19 17:42:30 +0800

    update baggage

--- scripts/insert/0insertBaggageType.sql
+++ scripts/insert/0insertBaggageType.sql
@@ -1,6 +1,14 @@
 INSERT ALL
-    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CHK', 'Check in baggage', 'Checked baggage at the airline’s check-in desk. Weighed before sending to the flight.')
-    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CO', 'Carry-on baggage', 'Cabin baggage is also known as carry-on or hand luggage, and can be a backpack or a small suitcase.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CHK', 'Check in baggage', 'Checked weight at the airlines check-in desk before sending to the flight.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CO', 'Carry-on baggage', 'carry-on or hand luggage, backpack or a small suitcase.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CO', 'Carry-on baggage', 'carry-on or hand luggage, backpack or a small suitcase.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('PIB', 'Personal item baggage', 'laptop bags, briefcases or camera bags')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CO', 'Carry-on baggage', 'carry-on or hand luggage, backpack or a small suitcase.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CHK', 'Check in baggage', 'Checked weight at the airlines check-in desk before sending to the flight.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('PIB', 'Personal item baggage', 'laptop bags, briefcases or camera bags')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('PIB', 'Personal item baggage', 'laptop bags, briefcases or camera bags')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CO', 'Carry-on baggage', 'carry-on or hand luggage, backpack or a small suitcase.')
+    INTO BAGGAGE_TYPE (baggage_type_id, baggage_type_name, baggage_type_description) VALUES ('CHK', 'Check in baggage', 'Checked weight at the airlines check-in desk before sending to the flight.')
 SELECT 1 FROM DUAL;
 
 
