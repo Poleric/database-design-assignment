646e7ba380172aa9c93e2af37655d6544e90e1ca
Poleric <nixonleongkb@gmail.com>
2024-04-27 13:23:48 +0800

    increase description length

--- scripts/create/0createSpecialService.sql
+++ scripts/create/0createSpecialService.sql
@@ -1,5 +1,5 @@
 CREATE TABLE SPECIAL_SERVICE (
     special_service_id           VARCHAR(4)    PRIMARY KEY,
     special_service_name         VARCHAR(30)   NOT NULL UNIQUE,
-    special_service_description  VARCHAR(200)
+    special_service_description  VARCHAR(300)
 );
