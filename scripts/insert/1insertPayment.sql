7f538dc69053e204ad03286e767704eeedf688cf
Poleric <nixonleongkb@gmail.com>
2024-04-30 19:22:36 +0800

    insert baggage and payment

--- scripts/insert/1insertPayment.sql
+++ scripts/insert/1insertPayment.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e98d62437abea02f30aeaf407a113186ea8ed65f732e2efbd1015636796048d
-size 20875778
+oid sha256:cd7ef4647f3b343bce92a468ef22193fb9add4cc5a7d0376c240faa47975e561
+size 49408254
---
+++ scripts/insert/3insertBaggage.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:924a3deb10605a25567ae7e5d94a8d462a25368e015d1ed554a460cd9af09669
+size 51149659
